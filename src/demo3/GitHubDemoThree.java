a40d182e97afa5b1c7077ed08d1f54869ac44770
dfeue <dfeue@WorkSmart.home>
2021-03-23 18:17:44 -0400

    Added another name

--- src/demo3/GitHubDemoThree.java
+++ src/demo3/GitHubDemoThree.java
@@ -5,6 +5,8 @@ public class GitHubDemoThree {
 	public static void main (String [] args) {
 		
 		System.out.println("Devora");
+		
+		System.out.println("Helen");
 	}
 	
 	//you added new code and now you have to share it with the team
